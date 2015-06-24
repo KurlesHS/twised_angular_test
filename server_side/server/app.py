4ee43ab4cf31c072251f7e22f4f9dca3584ef97c
KurlesHS <kahshs@gmail.com>
2015-06-24 07:05:19 +0300

    temp

--- server_side/server/app.py
+++ server_side/server/app.py
@@ -1,29 +1,14 @@
-from server import Server
-
-# from twisted.web.server import NOT_DONE_YET
-
-
 class App(object):
     def __init__(self, server):
         """
 
-        :type server: Server
+        :type server: klein.app.Klein
         """
         self.server = server
         self.setup_route(self.server)
 
     @staticmethod
     def setup_route(server):
-        @server.route('/hello/word')
+        @server.route('/hello/word', methods=['GET'])
         def test(request):
-
-            """
-
-            :type request: twisted.web.server.Request
-            """
-            if request.method == 'GET2':
-                request.setHeader('Custom', 'True')
-                return 'hello world'
-            else:
-                request.setResponseCode(404)
-                return 'error'
+            return 'help me plz!'
--- server_side/server_app.py
+++ server_side/server_app.py
@@ -1,9 +1,27 @@
+# coding=utf-8
 import os
-from server.server import Server
+
+from klein import Klein
+from twisted.internet import reactor
+from twisted.web.resource import Resource, ForbiddenResource
+from twisted.web.server import Site
+from twisted.web.static import File
+
 from server.app import App
+
+class FileNoDir(File):
+    def directoryListing(self):
+        return ForbiddenResource()
 
 if __name__ == '__main__':
     client_side_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'client_side'))
-    server = Server(client_side_dir)
+    server = Klein()
     app = App(server)
-    server.run()
+    root = server.resource()
+    site = Site(root)
+    # root.putChild("static", FileNoDir(os.path.join(client_side_dir, 'static')))
+    #root.putChild("app", FileNoDir(os.path.join(client_side_dir, 'app')))
+    #root.putChild("", File(os.path.join(client_side_dir, 'templates', 'index.html')))
+    #root.putChild("templates", File(os.path.join(client_side_dir, 'templates')))
+    reactor.listenTCP(8888, site)
+    reactor.run()
